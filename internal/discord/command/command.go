20241068225ad2e7b79718e220c846010074034a
DevMyong <myong.dev@gmail.com>
2021-09-28 01:08:27 +0900

    feat: command pattern

---
+++ internal/discord/command/command.go
@@ -0,0 +1,32 @@
+package command
+
+import "jake/internal/discord/config"
+
+type Commander interface {
+	Invokes() []string
+	Usage() string
+	Description() string
+
+	IsAdminRequired() bool
+
+	NArgs([]string) error
+	ValidArgs() config.ArgPattern
+	ValidFlags() config.FlagPattern
+
+	Exec(ctx *Context) error
+}
+
+type Command struct {
+	invokes     []string
+	usage       string
+	description string
+
+	isAdminRequired bool
+	nArgs           PositionalArgs
+
+	args       []string
+	flagMap    map[string]string
+	validArgs  config.ArgPattern
+	validFlags config.FlagPattern
+}
+type PositionalArgs func(args []string) error
