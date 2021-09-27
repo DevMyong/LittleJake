a6dd1762987c6b3f6d52ec0aee8d7720096684a6
DevMyong <myong.dev@gmail.com>
2021-09-28 01:04:48 +0900

    Feat: Connect Discord session

---
+++ cmd/discord/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"jake/internal/discord/command"
+	"jake/internal/discord/config"
+	"jake/internal/discord/events"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	const fileName = "../../config/config.json"
+
+	cfg, err := config.ParseConfigFromJSONFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	s, err := discordgo.New("Bot " + cfg.Token)
+	if err != nil {
+		panic(err)
+	}
+
+	s.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
+
+	registerEvents(s)
+	registerCommands(s, cfg)
+
+	if err = s.Open(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Bot is running, Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	s.Close()
+}
+
+func registerEvents(s *discordgo.Session) {
+	s.AddHandler(events.NewMessageHandler().Handler)
+}
+
+func registerCommands(s *discordgo.Session, cfg *config.Config) {
+	cmdHandler := command.NewCommandHandler(cfg.Prefix)
+	cmdHandler.OnError = func(err error, ctx *command.Context) {
+		_, err = ctx.Session.ChannelMessageSend(ctx.Message.ChannelID,
+			fmt.Sprintf("Command Execution failed: %s", err.Error()))
+	}
+
+	cmdHandler.RegisterCommand(command.NewUserInfo())
+	cmdHandler.RegisterMiddleware(&command.MwArguments{})
+	cmdHandler.RegisterMiddleware(&command.MwPermissions{})
+	s.AddHandler(cmdHandler.HandleMessage)
+}
