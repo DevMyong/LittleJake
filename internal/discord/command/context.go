f44c974155848b36278e5a673c18f61b813280b0
DevMyong <myong.dev@gmail.com>
2021-09-28 01:18:19 +0900

    Add: context

---
+++ internal/discord/command/context.go
@@ -0,0 +1,19 @@
+package command
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+// [prefix][cmd] [arg1] [arg2] [arg3] ....
+
+type Context struct {
+	Session *discordgo.Session
+	Message   *discordgo.Message
+	inputArgs []string
+	Handler   *CommandHandler
+
+	// Args is actual Arguments parsed from inputArgs
+	Args    []string
+	// FlagMap is actual Flags with parameter parsed from inputArgs
+	FlagMap map[string]string
+}
